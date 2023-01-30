51c9a4faac276f295b30650e30173ebc6b4ae3c9
gautam-mtalkz <122810723+gautam-mtalkz@users.noreply.github.com>
2023-01-30 18:24:19 +0530

    hi5

---
+++ php/week 2/day 9/07_date function in php.php
@@ -0,0 +1,46 @@
+<?php
+$date=date("T t S s d j D M x F");
+echo "Todays date is $date";
+echo "<br>";
+?>
+
+
+
+<!-- EXPERIMENT -->
+
+
+
+<?php
+$date=date("a b c d e f g h i j h k l m n o p  r s t u w v x y z");
+echo "Todays date is $date";
+echo "<br>";
+?>
+
+
+
+
+
+<?php
+$date=date("A B C D E F G H I J K L M N O P Q R S T W U X Y Z");
+echo "Todays date is $date";
+echo "<br>";
+?>
+
+<?php
+
+
+$timestamp = time();
+echo "Current timestamp: " . $timestamp . "<br>";
+
+
+?>
+
+
+
+ <!-- <?php
+var_dump(checkdate(01,21,-300));
+echo "<br>";
+var_dump(checkdate(12,19,2013));
+echo "<br>";
+
+?>  -->
---
+++ php/week 2/day 9/08_associative array.php
@@ -0,0 +1,14 @@
+<?php
+$myarr=array('Rahul'=>'Football', 'Ram'=>'Cricket', 'Sita'=>'Golf');
+foreach ($myarr as $key => $value) {
+    echo "$key like $value <br>";
+
+}
+?>
+
+
+<?php
+$myarr=array('Rahul'=>'Football', 'Ram'=>'Cricket', 'Sita'=>'Golf');
+echo $myarr['Rahul'];
+
+?>
